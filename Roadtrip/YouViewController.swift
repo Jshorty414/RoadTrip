bcec9ee689a50715ec7c3788c8aa053cbf672377
Kode With Klossy <kodewithklossy@MacBook-Air-3.local>
2018-08-08 15:02:34 -0500

    add the you info controller

---
+++ Roadtrip/YouViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YouViewController.swift
+//  Roadtrip
+//
+//  Created by Kode With Klossy on 8/8/18.
+//  Copyright © 2018 Kode With Klossy. All rights reserved.
+//
+
+import UIKit
+
+class YouViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+//
+//    @IBAction func continueTapped(_ sender: Any) {
+//        let username = nameInput.text!
+//          print(continueTapped)
+//    }
+//  
+    
+    
+
+}
