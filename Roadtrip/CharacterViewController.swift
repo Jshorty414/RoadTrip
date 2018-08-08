77d471f57cac12090be9ddd1a6fe1cba3d9bdb69
Kode With Klossy <kodewithklossy@MacBook-Air-3.local>
2018-08-08 12:32:18 -0500

    add functioning page view controller

---
+++ Roadtrip/CharacterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterViewController.swift
+//  Roadtrip
+//
+//  Created by Kode With Klossy on 8/8/18.
+//  Copyright © 2018 Kode With Klossy. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController{
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+   
+}
+
