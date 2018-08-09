d6ddf75a2cc7ed78edd8e22548ba9420cf2c9086
Kode With Klossy <kodewithklossy@MacBook-Air-3.local>
2018-08-09 14:10:01 -0500

    checkpoint 1

--- Roadtrip/YouViewController.swift
+++ Roadtrip/YouViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 class YouViewController: UIViewController {
     
     @IBOutlet weak var nameInput: UITextField!
-    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     //dismiss keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -31,25 +29,5 @@ class YouViewController: UIViewController {
     @IBAction func cytbTapped(_ sender: Any) {
         let username = nameInput.text!
         print(username)
-    }
-
-    @IBAction func option1(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "femaleStyle1")
-    }
-    
-    @IBAction func option2(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "maleStyle3")
-    }
-    
-    @IBAction func option3(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "otherStyle3")
-    }
-    
-    @IBAction func option4(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "femaleStyle2")
-    }
-    
-    @IBAction func option5(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "maleStyle1")
     }
 }
